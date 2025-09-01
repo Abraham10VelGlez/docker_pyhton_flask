cd91a037997588aad23f4821aa98721b66be250f
Abraham10VelGlez <usbrifa@gmail.com>
2025-09-01 16:10:08 -0600

    configuraciones extras2

--- src/app.py
+++ src/app.py
@@ -28,5 +28,5 @@ register_routex(app)
 
 # Start the Server
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=3000, debug=True)
-    app.run()
+    app.run(host="0.0.0.0", port=3000, debug=True)
+    
