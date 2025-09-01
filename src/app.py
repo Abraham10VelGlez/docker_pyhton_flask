6881616062a574ebe6fd84c78331cc67764a3ca5
Abraham10VelGlez <usbrifa@gmail.com>
2025-09-01 16:30:04 -0600

    configuraciones extras5

--- src/app.py
+++ src/app.py
@@ -28,5 +28,6 @@ register_routex(app)
 
 # Start the Server
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    port = int(os.environ.get("PORT", 3000))  # toma el puerto de Railway, si no existe usa 3000
+    app.run(host="0.0.0.0", port=port, debug=True)
     
